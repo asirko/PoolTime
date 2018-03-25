c93cf52d93d43a5d240f1678df4df891a50fed4f
asirko <alexandre@jump-biz.com>
2018-03-25 23:50:27 +0200

    Auto-generated commit

--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@ self.addEventListener('fetch', function(evt) {
 
 function precache() {
   return caches.open(CACHE).then(function (cache) {
-    debugger;
-    return cache.addAll([
-      './index.html'
-    ]);
+    return self.fetch('./file-to-cache.json?toto=' + Math.random()).then(res => {
+      console.log(res, res.json());
+      return cache.addAll(res.json());
+    });
   });
 }
 
