1a2c5f9353b85e31ff4a99905e2dfc33b933f303
asirko <alexandre@jump-biz.com>
2018-04-05 20:57:44 +0200

    Auto-generated commit

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE = 'cache-and-update';
 
 self.addEventListener('install', function(evt) {
   console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
+  evt.waitUntil(preCache());
 });
 
 self.addEventListener('fetch', function(evt) {
@@ -11,7 +11,7 @@ self.addEventListener('fetch', function(evt) {
   evt.waitUntil(update(evt.request));
 });
 
-function precache() {
+function preCache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
   "./3rdpartylicenses.txt",
@@ -22,7 +22,7 @@ function precache() {
   "./fontawesome-webfont.fee66e712a8a08eef580.woff",
   "./index.html",
   "./inline.318b50c57b4eba3d437b.bundle.js",
-  "./main.8a399109de524ca944ad.bundle.js",
+  "./main.0881260f35114429aab5.bundle.js",
   "./manifest.json",
   "./polyfills.bf95165a1d5098766b92.bundle.js",
   "./styles.a47a2984dd2d0fee3759.bundle.css",
@@ -47,59 +47,3 @@ function update(request) {
     });
   });
 }
-
-
-
-
-
-
-
-
-/* var CACHE = 'PoolTime';
-
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
-});
-
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  evt.respondWith(fromNetwork(evt.request, 5000)
-    .catch(function () {
-      return fromCache(evt.request);
-    }));
-});
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return self.fetch('./file-to-cache.json?toto=' + Math.random())
-      .then(res => { console.log(res); return res; })
-      .then(res => res.json())
-      .then(res => { console.log(res); return res; })
-      .then(files => cache.addAll(files));
-  });
-}
-
-function fromNetwork(request, timeout) {
-  console.log('From network')
-  return new Promise(function (fulfill, reject) {
-    var timeoutId = setTimeout(reject, timeout);
-
-    fetch(request).then(function (response) {
-      console.log('succeed from network');
-      clearTimeout(timeoutId);
-      fulfill(response);
-    }, reject);
-  });
-}
-
-function fromCache(request) {
-  console.log('failed from network');
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-*/
